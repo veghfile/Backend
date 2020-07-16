e093aee930f5e4c600cfe0c0b074275499d1fb51
devesh_pawar <devesh@iaspire.tech>
2020-07-17 02:16:29 +0530

    make it better

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ io.on('connection', socket => {
 
 });
 
-// server.listen(process.env.PORT || 8005, () => console.log('server is running on port 8000'));
+server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
 
-server.listen(8000, () => console.log('server is running on port 8000'));
+// server.listen(8000, () => console.log('server is running on port 8000'));
 
